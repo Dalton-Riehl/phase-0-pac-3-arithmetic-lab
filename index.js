ab2e23445e88da05be79de9099be4463759a4658
Dalton Riehl <dtonriehl@gmail.com>
2024-02-07 16:23:08 -0600

    Arithmetic Lab Complete: Achievement Get!

---
+++ index.js
@@ -0,0 +1,41 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+add(9, 10);
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+subtract(18, 7);
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+multiply(23, 3);
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+divide(60, 4);
+
+function increment(n) {
+    n++;
+    return n;
+}
+increment(50);
+
+function decrement(n) {
+    n--;
+    return n;
+}
+decrement(420);
+
+function makeInt(n) {
+    return parseInt(n, 10);
+}
+makeInt(335.951);
+
+function preserveDecimal(n) {
+    return parseFloat(n);
+}
+preserveDecimal(98.7654321);
